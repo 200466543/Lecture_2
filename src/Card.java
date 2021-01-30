625416f0d8154c3f7f6eb07df091b31bd20cb146
200466543 <200466543@student.georgianc.on.ca>
2021-01-30 13:46:33 -0500

    Added setSuit() method

---
+++ src/Card.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Card {
+    private String faceName;
+    private String suit;
+
+    /**
+     * This constructor takes 2 arguments, one for the face name and one for the suit.
+     * Aces are high, will dynamically establish card value with getCardValue()
+     */
+    public Card(String faceName, String suit){
+        this.faceName=faceName;
+        setSuit(suit);  //this uses the setSuit method made below instead of this. methods
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    public void setFaceName(String faceName) {
+        this.faceName = faceName;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        suit = suit.toLowerCase();//converts to lowercase
+        List<String> validSuits = Arrays.asList("hearts", "diamonds", "spades", "clubs");
+        if (validSuits.contains(suit))
+            this.suit = suit;
+        else
+            throw new IllegalArgumentException(suit + " is not valid. Use one of "+validSuits);
+    }
+
+    public String toString(){
+        return faceName + " of " + suit;
+    }
+}
---
+++ src/CardTester.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CardTester {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Card aceOfSpades = new Card("ace", "spades");
+
+        System.out.println(aceOfSpades);
+
+    }
+}
